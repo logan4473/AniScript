29a931913f8b96f5f27961506f0b368cd1bdfb37
Logan <girdharshukla01@gmail.com>
2023-03-02 21:35:28 +0530

    implemented literal DFA

--- Parser/Lexer/DFA/literal.h
+++ Parser/Lexer/DFA/literal.h
@@ -1,8 +1,19 @@
 #ifndef LITERAL_H
 #define LITERAL_H
 
-int literal(std::string& tokenString) {
-    return 0;
+bool literal(std::string& tokenString) {
+
+    std::string::iterator it = tokenString.begin();
+
+    if(*it!='\'') return false;
+    ++it;
+    
+    while(isalnum(*it)) ++it;
+
+    if(*it!='\'') return false;
+    ++it;
+
+    return (it==tokenString.end());
 }
 
 #endif
