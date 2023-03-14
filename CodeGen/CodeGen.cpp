bb628f74bcba013f66f6b37bd051714ddfe919a7
Logan <girdharshukla01@gmail.com>
2023-03-14 19:27:22 +0530

    Variable Expression Code Gen

--- CodeGen/CodeGen.cpp
+++ CodeGen/CodeGen.cpp
@@ -2,3 +2,11 @@ static std::unique_ptr<LLVMContext> TheContext;
 static std::unique_ptr<Module> TheModule;
 static std::unique_ptr<IRBuilder<>> Builder;
 static std::map<std::string, Value *> NamedValues;
+
+Value *VariableExprAST::codegen() {
+  // Look this variable up in the function.
+  Value *V = NamedValues[Name];
+  if (!V)
+    LogErrorV("Unknown variable name");
+  return V;
+}
