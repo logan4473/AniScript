f2c37ff5771e583812ea85bb37002bb10a184221
Logan <girdharshukla01@gmail.com>
2023-03-15 19:18:37 +0530

    Binary Expression Code Gen

--- CodeGen/CodeGen.cpp
+++ CodeGen/CodeGen.cpp
@@ -4,9 +4,31 @@ static std::unique_ptr<IRBuilder<>> Builder;
 static std::map<std::string, Value *> NamedValues;
 
 Value *VariableExprAST::codegen() {
-  // Look this variable up in the function.
   Value *V = NamedValues[Name];
   if (!V)
     LogErrorV("Unknown variable name");
   return V;
+}
+
+Value *BinaryExprAST::codegen() {
+  Value *L = LHS->codegen();
+  Value *R = RHS->codegen();
+  if (!L || !R)
+    return nullptr;
+
+  switch (Op) {
+  case '+':
+    return Builder->CreateFAdd(L, R, "addtmp");
+  case '-':
+    return Builder->CreateFSub(L, R, "subtmp");
+  case '*':
+    return Builder->CreateFMul(L, R, "multmp");
+  case '<':
+    L = Builder->CreateFCmpULT(L, R, "cmptmp");
+    
+    return Builder->CreateUIToFP(L, Type::getDoubleTy(TheContext),
+                                 "booltmp");
+  default:
+    return LogErrorV("invalid binary operator");
+  }
 }
